89545efd3a5eee58d8b587e4e1bdcefac4a7a531
stylopm <apmurillo99@hotmail.com>
2022-11-30 20:59:36 -0600

    feat: Se actualiza el ico

--- src/Components/pages/Address.js
+++ src/Components/pages/Address.js
@@ -1,4 +1,14 @@
 const Address = () => {
-    return <h1> Addres</h1>
+    return (<> 
+
+
+    
+
+            <iframe  width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=19.4935247, -99.1465932&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" 
+
+        frameBorder="0" scrolling="no" marginheight="0" marginwidth="0"
+        
+        > </iframe>
+    </>);
 }
 export default Address;

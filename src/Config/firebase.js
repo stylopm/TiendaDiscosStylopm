e9278505c08c8b14214ecb165f63a78622edbbd5
stylopm <apmurillo99@hotmail.com>
2022-12-01 19:21:04 -0600

    feat: Update files

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -9,21 +9,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-//   apiKey: process.REACT_APP_API_KEY,
-//   authDomain: process.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.REACT_APP_PROJECT_ID,
-//   storageBucket: process.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.REACT_APP_APP_ID,
-//   measurementId: process.REACT_APP_MEASUREMENT_ID,
-
-  apiKey: "AIzaSyCaCdQFpw68fITJG7IskCpZXQm9ee-PJfA",
-  authDomain: "stylopm-two.firebaseapp.com",
-  projectId: "stylopm-two",
-  storageBucket: "stylopm-two.appspot.com",
-  messagingSenderId: "1017225349078",
-  appId: "1:1017225349078:web:ca279da730f3146c41c677",
-  measurementId: "G-0T63VN40XP",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

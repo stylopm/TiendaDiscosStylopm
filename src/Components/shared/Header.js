eebc9e84558ed5192b1b2a98899f16948e0b6c29
stylopm <apmurillo99@hotmail.com>
2022-12-01 20:23:08 -0600

    feat: Update files

--- src/Components/shared/Header.js
+++ src/Components/shared/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <Navbar bg="dark" variant="dark">
       <Container>
         <LinkContainer to="/">
-          <Navbar.Brand>Tienda de discos STYLOPM</Navbar.Brand>
+          <Navbar.Brand> Tienda de discos STYLOPM </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">

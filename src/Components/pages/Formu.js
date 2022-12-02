e6792b99e82c1f6bd35ad23268540d05bde8b6e6
stylopm <apmurillo99@hotmail.com>
2022-12-01 21:30:26 -0600

    feat: Update files

--- src/Components/pages/Formu.js
+++ src/Components/pages/Formu.js
@@ -1,28 +1,67 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { addDoc, collection } from "firebase/firestore";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { db } from "./../../config/firebase";
+import { useState } from "react";
 
 const Formu = () => {
-    return (
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-    
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      );
-}
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const coment = collection(db, "contacts");
+      const res = await addDoc(coment, inputs);
+      alert('Se a registrado el el comentario')
+      return res;
+    } catch (e) {
+      console.error("Hubo un error", e);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicName">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control
+          onChange={handleChange}
+          name="nameForm"
+          type="text"
+          placeholder="Escriba su nombre"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Correo</Form.Label>
+        <Form.Control
+          onChange={handleChange}
+          name="emailForm"
+          type="email"
+          placeholder="Correo"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicDescription">
+        <Form.Label>Comentario</Form.Label>
+        <Form.Control
+          onChange={handleChange}
+          name="emailDescription"
+          type="text"
+          placeholder="Comentario"
+        />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+    </Form>
+  );
+};
 export default Formu;
